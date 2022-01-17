27317ad82d2653abee0ce309313f67d77599b8b4
vasylkucherak <vasil.kucherak@gmail.com>
2022-01-17 21:58:29 +0200

    v.0.1.4 (added empliyees-list)

---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,14 @@
+import EmployeesListItem from '../employees-list-item/employees-list-item';
+import './employees-list.css';
+
+const EmployessList = () => {
+    return (
+        <ul className="app-list list-group">
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+            <EmployeesListItem/>
+        </ul>
+    );
+}
+
+export default EmployessList;
