1564a74e40295417268e44c2f45a4ba08bd854c4
vasylkucherak <vasil.kucherak@gmail.com>
2022-01-17 20:07:17 +0200

    v.0.1.0 (added first component)

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting of employees in company N</h1>
+            <h2>Total number of employees:</h2>
+            <h2>The award will be given to:</h2>
+        </div>
+    );
+}
+
+export default AppInfo;
