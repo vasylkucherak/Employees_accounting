f85ccd103bd8480ae0275ae63147f5e13d6322c0
vasylkucherak <vasil.kucherak@gmail.com>
2022-01-18 22:53:39 +0200

    v.0.7.0 (added a function for submit new item in an employees list)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,6 +18,7 @@ class App extends Component {
                 {name: 'Carl W.', salary: 5000, increase: false, id: 3},
             ]
         }
+        this.maxId = 4;
     }
 
     deleteItem = (id) => {
@@ -26,6 +27,21 @@ class App extends Component {
                 data: data.filter(item => item.id !== id)
             }
         })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
     }
 
     render() {
@@ -42,7 +58,7 @@ class App extends Component {
                     data={this.state.data}
                     onDelete={this.deleteItem}/>
     
-                <EmployeesAddForm/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
     }
