e4143aa40cf66434ff748a316b1a6c3b83c7002e
vasylkucherak <vasil.kucherak@gmail.com>
2022-01-18 23:51:27 +0200

    v.0.8.0 (created a counter in app-info)

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = ({increased, employees}) => {
     return (
         <div className="app-info">
             <h1>Accounting of employees in company N</h1>
-            <h2>Total number of employees:</h2>
-            <h2>The award will be given to:</h2>
+            <h2>Total number of employees: {employees}</h2>
+            <h2>The award will be given to: {increased}</h2>
         </div>
     );
 }
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,7 +1,7 @@
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css';
 
-const EmployessList = ({data, onDelete}) => {
+const EmployessList = ({data, onDelete, onToggleProp}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
@@ -9,7 +9,8 @@ const EmployessList = ({data, onDelete}) => {
             <EmployeesListItem 
                 key={id}
                 {...itemProps}
-                onDelete={() => onDelete(id)}/>
+                onDelete={() => onDelete(id)}
+                onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/>
         )
     })
 
